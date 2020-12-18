a769e42097b6e5158f1014a6eba16d02905f5c8e
kijk2869 <mareowst@gmail.com>
2020-12-19 03:17:56 +0900

    위치 정보 기반 시설 찾기 알고리즘 구축
    Co-authored-by: SaidBySolo <SaidBySolo@users.noreply.github.com>

---
+++ nearest.py
@@ -0,0 +1,17 @@
+from typing import Any, Dict, Tuple
+import pandas
+import math
+
+OpenDataSet = {
+    "전국건강증진센터표준데이터": pandas.read_csv("./opendata/전국건강증진센터표준데이터.csv"),
+    "정신건강관련전체기관정보": pandas.read_csv("./opendata/정신건강관련전체기관정보_2019..csv")
+}
+
+
+def getNearest(Pos: Tuple[int, int], type: str) -> Dict[str, Any]:
+    DataSet = OpenDataSet[type]
+
+    return sorted(
+        map(lambda x: x[1], DataSet.iterrows()),
+        key=lambda x: math.dist(x[["위도", "경도"]].values.tolist(), Pos)
+    )[0].to_dict()
