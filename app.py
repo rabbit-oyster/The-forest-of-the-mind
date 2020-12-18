d10064f6d5917f930cb02cda18c50702e9c8cd1d
kijk2869 <mareowst@gmail.com>
2020-12-19 00:19:38 +0900

    진단 검사 기여 규칙 제정

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from sanic import Sanic, response
 from sanic_session import Session, InMemorySessionInterface
-from socketio import AsyncServer as AsyncioSocketIOServer
+import socketio
 
 app = Sanic(__name__)
 session = Session(app, interface=InMemorySessionInterface())
-socketio = AsyncioSocketIOServer(async_mode='sanic')
-socketio.attach(app)
+sio = socketio.AsyncServer(async_mode='sanic')
+sio.attach(app)
 
-@app.route("/")
-async def index(request):
-    ...
+sessionSidStorage = {}
+
+@app.middleware("response")
+async def allowCors(_, response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+
+@sio.event
+async def connect(sid, environ):
+    request = environ["sanic.request"]
+
+    for sid, _ in list(filter(lambda x: x[1] == request.ctx.session.sid, sessionSidStorage.items())):
+        del sessionSidStorage[sid]
+
+    sessionSidStorage[sid] = request.ctx.session.sid
+
+@sio.event
+async def disconnect_request(sid):
+    await sio.disconnect(sid)
+
+@sio.event
+async def disconnect(sid):
+    if sid in sessionSidStorage.keys():
+        del sessionSidStorage[sid]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, auto_reload=True)
